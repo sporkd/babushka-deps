c9d2dbea961d9f03ad79ff542117425fb402dcd6
Peter Gumeson <gumeson@gmail.com>
2012-06-17 23:39:20 -0700

    Uncommented remaining requirements

--- env.rb
+++ env.rb
@@ -1,17 +1,12 @@
 dep 'env', :github_username do
   username = shell('whoami')
 
-  if confirm("Install xcode commandline tools", :default => 'y')
-    requires 'xcode commandline tools'
-  else
-    requires 'xcode tools'
-  end
-
-  # requires 'osx prefs'
-  # requires 'packages'
-  # requires 'dotfiles'
-  # requires 'osx applications'
-  # requires 'benhoskings:zsh'.with(username)
+  requires 'xcode commandline tools'
+  requires 'osx prefs'
+  requires 'packages'
+  requires 'dotfiles'
+  requires 'osx applications'
+  requires 'benhoskings:zsh'.with(username)
 
   after {
     log_ok "Done. Your env is ready!"
